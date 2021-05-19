bf4d250673fc2e47d1f724e9344fb5d3f5966059
jbeen2 <dlwoqls4323@gmail.com>
2021-05-19 22:51:08 +0900

    21.05.19

--- DynamicProgramming/BJ2156.py
+++ DynamicProgramming/BJ2156.py
@@ -1,3 +1,10 @@
 n = int(input())
 wine = [int(input()) for _ in range(n)]
+dp = [0, wine[0]]
 
+if n > 1 :
+    dp.append(wine[0]+wine[1])
+    for i in range(3, n+1) :
+        dp.append(max(dp[i-1], dp[i-2]+wine[i-1], dp[i-3]+wine[i-1]+wine[i-2]))
+
+print(dp[-1])
