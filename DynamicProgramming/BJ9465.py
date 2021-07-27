5ad44e9e6de6d2c0ba9b5261524ff80859c2131e
jbeen2 <dlwoqls4323@gmail.com>
2021-07-27 16:05:00 +0900

    21.07.27

--- DynamicProgramming/BJ9465.py
+++ DynamicProgramming/BJ9465.py
@@ -3,18 +3,11 @@ for _ in range(T) :
     n = int(input())
     sticker = [list(map(int, input().split())) for _ in range(2)]
 
-    prev= [[0]*n for _ in range(2)]
+    if n > 1:   # ^_^ ..
+        sticker[0][1] += sticker[1][0] ; sticker[1][1] += sticker[0][0]
 
-    prev[0][0] = sticker[0][0]
-    prev[1][0] = sticker[1][0]
+        for i in range(2, n) :
+            sticker[0][i] += max(sticker[1][i-1], sticker[1][i-2])
+            sticker[1][i] += max(sticker[0][i-1], sticker[0][i-2])
 
-    prev[0][1] = prev[1][0] + sticker[0][1]
-    prev[1][1] = prev[0][0] + sticker[1][1]
-
-
-    for i in range(2, n) :
-        prev[0][i] = max(prev[1][i-1], prev[1][i-2])+sticker[0][i]
-        prev[1][i] = max(prev[0][i-1], prev[0][i-2])+sticker[1][i]
-
-
-    print(max(prev[0][n-1], prev[1][n-1]))
+    print(max(sticker[0][n-1], sticker[1][n-1]))
