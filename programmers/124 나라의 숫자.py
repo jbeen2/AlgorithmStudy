82851391578e982f0a2823ffb4a78dcf168b6eca
jbeen2 <dlwoqls4323@gmail.com>
2021-08-06 15:34:58 +0900

    21.08.06

--- programmers/124 나라의 숫자.py
+++ programmers/124 나라의 숫자.py
@@ -1,37 +1,42 @@
-def solution(n) :
-    i, k = 1, 3
-    m = n
-    number = [1,2,4]
+def solution(n):
+    if n<=3:
+        return '124'[n-1]
+    else:
+        q, r = divmod(n-1, 3)
+        return solution(q) + '124'[r]
 
-    if n <= 3 :
-        return number[n-1]
 
-    while m > k :
-        i += 1
-        m -= k
+# 효율성 ㅠ_ㅠ 시간 초과 ~~~!!
+
+def solution2(n):
+    num = ['1', '2', '4']
+    k = 3
+    while n >= k :
+        tmp1 = ['1'+i for i in num[-k:]]
+        tmp2 = ['2'+i for i in num[-k:]]
+        tmp4 = ['4'+i for i in num[-k:]]
+
+        num.extend(tmp1)
+        num.extend(tmp2)
+        num.extend(tmp4)
+
         k *= 3
-
-        if m <= k :
-            answer = []
-
-            print(m)
-            print(k)
-            print(i)    # 3 : 0 1 2
-            print("===")
-            print(3**(i-1))
-            print((m-1)//(3**(i-1)))
-            print("===")
+    return num[n-1]
 
 
-            new_m = m
-            for num in range(i-1, -1, -1) :   # 2 1 0
-                print(num)
-                print(3**num)
-                print((new_m-1) // (3**num))
-                new_m -= (3 ** num)
-                print(new_m)
-                print('---')
-            print(new_m)
+def solution3(n) :
+    # answer = [''] * (n+1)
+    # if n <= 3 :
+    #     answer[n] = '124'[n-1]
+    # else :
+    #     q, r = divmod(n-1, 3)
+    #     answer[n] = answer[q] + '124'[r]
 
-n = int(input())
-print(solution(n))
+    answer = ['', '1', '2', '4'] + ['']*(n)
+    for i in range(4, n+1) :
+        q, r = divmod(i-1, 3)
+        answer[i] = answer[q] + '124'[r]
+    return answer[n]
+
+
+print(solution(2))
